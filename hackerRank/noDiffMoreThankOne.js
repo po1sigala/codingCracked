a7e445c918c7c960f2a3b11fee55ad85907b67a3
Your Name <alexsigala87@gmail.com>
2020-02-11 16:36:03 -0800

    solved for finding smallest subest of intes where absolute diff is one or 0

---
+++ hackerRank/noDiffMoreThankOne.js
@@ -0,0 +1,26 @@
+//given an array return max num of integers you can select where dif <= 1
+
+//my area can only contain a which are within one digit of each other
+
+const pickinga = (b, a) => {
+    a = a.split(" ");
+    console.log(a);
+    let longest = [];
+
+    a.map(int => {
+        let test = [];
+
+        for (i = 0; i < a.length; i++) {
+            if (int - a[i] === 0 || int - a[i] === 1) {
+                test.push(a[i]);
+            }
+        }
+
+        if (test.length > longest.length) {
+            longest = test;
+        }
+    });
+    return longest.length;
+};
+
+console.log(pickinga(6, "1 2 3 3 4"));
