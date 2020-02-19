b3f69dc3d20656d4084279acda65b56ce4981715
Your Name <alexsigala87@gmail.com>
2020-02-19 10:33:29 -0800

    working on divising problem works for most cases but not when dividsor is -1

--- leetcodeProblems/divideints.js
+++ leetcodeProblems/divideints.js
@@ -1,22 +1,27 @@
 //divide two numbers without using multiplication division and mod operator
 var divide = function(dividend, divisor) {
     console.log("trying");
+    abs = Math.abs(divisor);
 
     //to divide by 5 for example th edividend must be it plus itsels dividend times
     let quotient = 0;
     let sum = 0;
     //quotient equlas the number of times i must add the divisor to equal the dividend
-    for (i = 0; i < divisor + 1; i++) {
-        console.log(
-            `sum is ${sum} divisor is ${divisor} quotent is ${quotient}`
-        );
-        sum += divisor;
-        if (sum > dividend) {
+    for (i = 0; i < abs + 1; i++) {
+        console.log(`sum is ${sum} divisor is ${abs} quotent is ${quotient}`);
+        sum += abs;
+        if (sum > Math.abs(dividend)) {
             console.log(`${sum} is bigger than ${dividend}`);
-            return quotient;
+            if (divisor < 0 && dividend < 0) {
+                return quotient;
+            } else if (divisor < 0 || dividend < 0) {
+                return -quotient;
+            } else {
+                return quotient;
+            }
         }
         quotient += 1;
     }
 };
 
-console.log(divide(15, 5));
+console.log(divide(-2147483648, -1));
