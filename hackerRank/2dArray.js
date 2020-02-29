ed66af9b6d635c8696490b1d0ab663cf1282a420
Your Name <alexsigala87@gmail.com>
2020-02-28 18:35:13 -0800

    added new hackerank algo

---
+++ hackerRank/2dArray.js
@@ -0,0 +1,30 @@
+//calculate all hour glasses in a 2darry that is 6 by six
+/*
+given:
+123
+456
+789
+
+hour glass is 123
+               5
+              789
+maximum output is 35 bc only one hour glass
+
+in a 6by 6 array there will be 16 possible
+*/
+
+function hourglassSum(arr) {
+    let largestSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        switch (i) {
+            case 0:
+
+            case 1:
+            case 2:
+            case 3:
+            default:
+                console.log("oops");
+        }
+    }
+    return largestSum;
+}
---
+++ hackerRank/plusminusEasy.js
@@ -0,0 +1,10 @@
+/*
+given array of ints calculte fraction that are positive negative or zero
+return decimal verion
+*/
+function plusMinus(arr) {
+    let total = arr.length;
+    let positive = 0;
+    let negative = 0;
+    let zeroes = 0;
+}
