e6bd7d6f837a4edd4b76e95a14139eb423d0584c
Your Name <alexsigala87@gmail.com>
2020-03-01 19:44:32 -0800

    started solution for arry problem

--- hackerRank/plusminusEasy.js
+++ hackerRank/plusminusEasy.js
@@ -7,4 +7,16 @@ function plusMinus(arr) {
     let positive = 0;
     let negative = 0;
     let zeroes = 0;
+    for (let i = 0; i < total; i++) {
+        if (arry[i] === 0) {
+        } else if (arry[i] < 0) {
+        } else if (arr[i] > 0) {
+        } else {
+            console.log(
+                `something weird is happening with the array at i it is ${arr[i]}`
+            );
+        }
+    }
 }
+
+console.log(plusMinus([1, 1, 0, -1, -2]));
